acd14cb131e9502bf857ee503db1a6da7fb632ec
liutong <jackie6053@outlook.com>
2018-10-19 14:19:02 +0800

    add thread

---
+++ src/main/java/com/ltpc/study/core/thread/MyRunnable.java
@@ -0,0 +1,12 @@
+package com.ltpc.study.core.thread;
+
+public class MyRunnable implements Runnable {
+	private int ticket = 5;
+	@Override
+	public void run() {
+		for(int i=0;i<10 && ticket>0;i++){
+			System.out.println("ticket = "+ ticket--);
+		}
+	}
+
+}
---
+++ src/main/java/com/ltpc/study/core/thread/TryConcurrency.java
@@ -0,0 +1,41 @@
+package com.ltpc.study.core.thread;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: liutong
+ * Date: 2018/8/28
+ * Time: 下午5:58
+ * Description:
+ **/
+public class TryConcurrency {
+
+    public static void main(String[] args) {
+        new Thread(TryConcurrency::enjoyMusic).start();
+        browseNews();
+    }
+
+    private static void browseNews(){
+        while (true){
+            System.out.println(LocalDateTime.now()+" Un-huh,the good news");
+            sleep(1);
+        }
+    }
+
+    private static void enjoyMusic(){
+        while (true){
+            System.out.println(LocalDateTime.now()+" Uh-huh,the nice music");
+            sleep(1);
+        }
+    }
+
+    private static void sleep(int seconds){
+        try {
+            TimeUnit.SECONDS.sleep(seconds);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
