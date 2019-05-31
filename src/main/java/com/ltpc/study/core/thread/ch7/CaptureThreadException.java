ac12068acc099f9b6b0c7b2ec2c8eac7f67f7063
liutong <jackie6053@outlook.com>
2019-05-31 17:12:26 +0800

    添加ch7实例。

---
+++ src/main/java/com/ltpc/study/core/thread/ch7/CaptureThreadException.java
@@ -0,0 +1,34 @@
+package com.ltpc.study.core.thread.ch7;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: liutong
+ * Date: 2019-05-31
+ * Time: 16:43
+ * Description:
+ **/
+public class CaptureThreadException {
+
+
+    public static void main(String[] args) {
+        // 设置默认异常处理。
+        Thread.setDefaultUncaughtExceptionHandler((t,e)->{
+            System.out.println(t.getName()+" occur exception");
+            e.printStackTrace();
+        });
+
+        final Thread thread = new Thread(()->{
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            // 用于抛出异常
+            System.out.println(1/0);
+        },"Test-thread");
+
+        thread.start();
+    }
+}
