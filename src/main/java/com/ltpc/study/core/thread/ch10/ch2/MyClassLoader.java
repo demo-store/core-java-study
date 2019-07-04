7969116363e283449bd220ea5f83aca7eb448c51
liutong <jackie6053@outlook.com>
2019-07-04 11:39:13 +0800

    add ch10.2

---
+++ src/main/java/com/ltpc/study/core/thread/ch10/ch2/MyClassLoader.java
@@ -0,0 +1,12 @@
+package com.ltpc.study.core.thread.ch10.ch2;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author liutong
+ * @date 2019-06-24 10:00
+ * Description:
+ **/
+public class MyClassLoader extends ClassLoader {
+
+}
